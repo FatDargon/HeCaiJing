509b42e4d189c446a3af82d09466665933c053bf
FatDargon <zhujmhh@qq.com>
2017-09-27 20:56:22 +0800

    yhhhh
    iii

---
+++ hcj/caijing_hangye.py
@@ -0,0 +1,86 @@
+# -*- coding:utf-8 -*-  
+'''
+Created on 2017年9月27日
+
+@author: Administrator
+'''
+# -*- coding:UTF-8 -*-
+'''
+Created on 2017-9-17
+
+@author: Administrator
+'''
+from tools.GetSoup import *
+from dateutil import parser
+import re
+import jieba
+import jieba.posseg
+import jieba.analyse
+from tools.Get_html import *
+#url = 'http://stock.caijing.com.cn/stockplate/'
+def caijing_hangye(url,name):
+    name = name.decode('utf-8')
+    big_data=[]
+    soup=get_html()
+#     soup = Soup(url,'utf-8').get_soup()
+    print soup
+    news_list = soup.find('div',class_='main_lt').ul.find_all('li')
+    for news in news_list:
+        print news.get_text(strip = True)
+        exit()
+        time_str = news.find('p',class_='bd_i_time_c').get_text(strip = True)
+        datetime_struct = parser.parse(time_str)
+        time = datetime_struct.strftime('%Y-%m-%d %H:%M:%S')
+#         print 'time \t'+datetime_struct.strftime('%Y-%m-%d %H:%M:%S') # 2016-12-22 13:58:59
+        content =  news.find('div',class_='bd_i_txt').get_text(strip = True)
+#         re_title =re.compile()
+#         print content
+        try:
+            news_source_tmp = re.search(u'（.+）', content).group()
+            news_source =re.sub(u'[（）]', u'', news_source_tmp)
+#             print 'news_source\t'+news_source
+            content = re.sub(u'（.+）','',content)
+        except:
+#             print 'news_source\tnone'
+            news_source = u'无'
+        try:
+            title_tmp = re.search(u'【.+】', content).group()
+            title = re.sub(u'[【】]', '', title_tmp)
+#             print 'title\t'+title
+            text = re.sub(title_tmp, '', content)
+#             print 'text\t'+text
+        except:
+            title = u'无'
+            text = content
+#             print "title\tnone"
+#             print 'title\t'+content
+        tags = jieba.analyse.extract_tags(content,topK=3, withWeight=False,allowPOS=('ns', 'n'))
+#         for x, w in jieba.analyse.extract_tags(content, withWeight=True):
+#             print('%s %s' % (x, w))
+#             exit()
+        keywords = u'/'.join(tags)
+#         print keywords
+        small_data = {
+            'title':title,
+            'author':news_source,
+            'time':time,
+            'source':name,#sina finance
+            'type':'1',
+            'keywords':keywords,
+            'viewer':'0',
+            'score':'0',     
+            'content':text   
+        }
+        big_data.append(small_data) 
+        small_data={
+            'title':'',
+            'author':'',
+            'time':'',
+            'source':'',#sina finance
+            'type':'',
+            'keywords':'',
+            'viewer':'',
+            'score':'',
+            'content':''   
+        }
+    return big_data
--- test/test_for_read_file.py
+++ test/test_for_read_file.py
@@ -6,16 +6,16 @@ Created on 2017-9-17
 '''
 from tools.get_text import read_file
 from tools.myprint import *
-from hcj.sina_finance import *
+from hcj.caijing_hangye import *
 from tools.To_Mysql import ToMysql
 now = datetime.now()
 now = now.strftime("%Y-%m-%d %H:%M:%S")
 obj = read_file()
 # pretty_list(obj)
-url = obj[0]['source_url']
-name = obj[0]['source_name']
+url = obj[1]['source_url']
+name = obj[1]['source_name']
 print url
-ans= sina_finance(url,name)
+ans= caijing_hangye(url,name)
 # ans =[{
 #             'title':"title",
 #             'author':"news_source",
@@ -27,9 +27,9 @@ ans= sina_finance(url,name)
 #             'score':'0',     
 #             'content':u"發垃圾分類登記法"   
 #         }]
-cur = ToMysql()
-cur.insert_list(ans)
-cur.commit()
+# cur = ToMysql()
+# cur.insert_list(ans)
+# cur.commit()
 # pretty_list(ans)
 
 # for i in obj:
